93c6dceea4cfa76d557bab7317fd2e92db60e1d1
赵子良 <ziliang.zhao@enwaysoft.com>
2018-04-04 09:46:04 +0800

    第一次提交

---
+++ app/src/main/java/zzl/com/eventbustest/MainActivity.java
@@ -0,0 +1,13 @@
+package zzl.com.eventbustest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
